f65ef75858506f082355d467bb45cb8ca970ee32
Hazdryx <hazdryx@gmail.com>
2020-09-20 20:09:22 -0500

    Created 10 second sleep timer.

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { exec } = require('child_process');
+const { SSL_OP_NO_TICKET } = require('constants');
+
+/**
+ * @type {BrowserWindow}
+ */
+let mainWindow = null;
 
 /**
  * Creates the main window.
  */
-function createWindow() {
-    let win = new BrowserWindow({
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 800,
         webPreferences: {
             nodeIntegration: true,
         }
     });
+    mainWindow.loadFile('index.html');
 
-    win.loadFile('index.html');
+    // Start first tick.
+    tick();
+}
+/**
+ * Sends a tick message every second.
+ */
+function tick() {
+    mainWindow.webContents.send('tick');
+    setTimeout(tick, 1000);
 }
 
 // Only create window when ready.
-app.whenReady().then(createWindow);
-
-// Listen to shutdown channel
-ipcMain.on("shutdown", () => {
-    console.log("HELLO WORLD!");
-    exec('shutdown /s /t 0');
+app.whenReady().then(() => {
+    createMainWindow();
+    SSL_OP_NO_TICKET
 });
+// Listen to shutdown channel
+ipcMain.on("shutdown", () => exec('shutdown /s /t 0'));
--- renderer.js
+++ renderer.js
@@ -3,8 +3,32 @@
  */
 const { ipcRenderer } = require('electron');
 
-// Shutdown on button click.
-document.querySelector("#btn_shutdown").addEventListener("click", () => {
-    console.log("sending shutdown event");
-    ipcRenderer.send("shutdown");
+// Time constans
+const MILLIS_PER_MINUTE = 60000;
+
+// Timer constants
+const TIMER_DURATION = 10000;
+const TIMER_GRACE_PERIOD = 0;
+
+// If this is -1, timer is not running.
+let timerStart = -1;
+
+// Start timer / Reset timer on button click.
+document.querySelector("#btn_timer").addEventListener("click", function() {
+    this.innerText = "Reset Timer";
+    timerStart = new Date().getTime();
+
+    tick();
 });
+
+// Called when main process calls tick.
+function tick() {
+    if (timerStart < 0) return;
+
+    let timeLeft = TIMER_DURATION - (new Date().getTime() - timerStart);
+    if (timeLeft <= -TIMER_GRACE_PERIOD) ipcRenderer.send("shutdown");
+    
+    let renderTime = (timeLeft < 0) ? 0 : timeLeft;
+    document.querySelector("#time_left").innerText = Math.ceil(renderTime / 1000) + " seconds";
+}
+ipcRenderer.on("tick", tick);
