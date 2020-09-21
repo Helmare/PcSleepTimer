7ef75b2fd6b18dc5563d97a787e8a97ce7acb889
Hazdryx <hazdryx@gmail.com>
2020-09-20 20:39:29 -0500

    Moved timer code to SleepTimer class.

--- renderer.js
+++ renderer.js
@@ -3,31 +3,22 @@
  */
 const { ipcRenderer } = require('electron');
 
-// Time constans
-const MILLIS_PER_MINUTE = 60000;
-
-// Timer constants
-const TIMER_DURATION = 10000;
-const TIMER_GRACE_PERIOD = 0;
-
-// If this is -1, timer is not running.
-let timerStart = -1;
+let timer = new SleepTimer();
+timer.duration(10);
 
 // Start timer / Reset timer on button click.
 document.querySelector("#btn_timer").addEventListener("click", function() {
-    this.innerText = "Reset Timer";
-    timerStart = new Date().getTime();
-
+    timer.start();
+    timer.reset();
     tick();
 });
 
 // Called when main process calls tick.
 function tick() {
-    if (timerStart < 0) return;
-
-    let timeLeft = TIMER_DURATION - (new Date().getTime() - timerStart);
-    if (timeLeft <= -TIMER_GRACE_PERIOD) ipcRenderer.send("shutdown");
+    if (!timer.isActive()) return;
+    if (timer.isFinished()) ipcRenderer.send("shutdown");
     
+    let timeLeft = timer.timeLeft();
     let renderTime = (timeLeft < 0) ? 0 : timeLeft;
     document.querySelector("#time_left").innerText = Math.ceil(renderTime / 1000) + " seconds";
 }
---
+++ sleeptimer.js
@@ -0,0 +1,66 @@
+/**
+ * A sleep timer class which can start
+ * and stop sleep timers.
+ */
+class SleepTimer {
+    constructor() {
+        this.duration = 10000;
+        this.startTime = -1;
+    }
+
+    /**
+     * Gets or sets the duraiton in seconds.
+     * @param {number} seconds 
+     */
+    duration(seconds) {
+        if (seconds === undefined) {
+            return this.duration / 1000;
+        }
+        else {
+            this.duration = seconds * 1000;
+        }
+    }
+    /**
+     * @returns whether this timer is active.
+     */
+    isActive() {
+        return this.startTime >= 0;
+    }
+    /**
+     * Gets the time left on this timer.
+     * @returns the number of seconds left on the timer or -1 if inactive.
+     */
+    timeLeft() {
+        if (this.isActive())
+            return this.duration - (new Date().getTime() - this.startTime);
+        else
+            return -1;
+    }
+    /**
+     * @returns whether the timer is finished.
+     */
+    isFinished() {
+        return this.timeLeft() <= 0;
+    }
+
+    /**
+     * Starts the sleep timer.
+     */
+    start() {
+        if (this.isActive()) return;
+        this.startTime = new Date().getTime();
+    }
+    /**
+     * Resets the sleep timer.
+     */
+    reset() {
+        if (!this.isActive()) return;
+        this.startTime = new Date().getTime();
+    }
+    /**
+     * Stops the sleep timer.
+     */
+    stop() {
+        this.startTime = -1;
+    }
+}
